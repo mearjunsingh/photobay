d90e5e03c027402adf1f08adafc824552a32e047
Arjun Singh <admin@arjunsingh.com.np>
2020-09-03 22:20:34 +0545

    Templates and Static Files Setup

---
+++ my_CDN/js/script.js
@@ -0,0 +1,204 @@
+//to open and close mobile menu on click
+function mobileNav() {
+  var x = document.getElementById("mobileNav");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+
+
+//for explore menu on click
+function explore() {
+  var x = document.getElementById("explore");
+  if (x.className.indexOf("w3-show") == -1) { 
+      x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+
+
+//for account box
+var modal = document.getElementById('account_box');
+//for register box
+var rmodal = document.getElementById('register_box');
+
+//open on click
+function account_box_open() {
+    modal.style.display='block';
+}
+
+
+//close on cross click
+function box_close() {
+    modal.style.display='none';
+    rmodal.style.display='none';
+}
+
+
+
+//close on anywhere click
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  if (event.target == rmodal) {
+    rmodal.style.display = "none";
+  }
+}
+
+
+function goto_login_box() {
+    rmodal.style.display='none';
+    modal.style.display='block';
+}
+
+function goto_register_box() {
+    rmodal.style.display='block';
+    modal.style.display='none';
+}
+
+
+
+
+//year for footer
+var footeryear = new Date().getFullYear();
+document.getElementById("footeryear").innerHTML = footeryear;
+
+
+
+
+
+//category filter in discover page
+
+// Dropdown
+function cat_filter_drop() {
+  var x = document.getElementById("cat_filter");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Filter
+function cat_filter() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("cat_filter_input");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("cat_filter");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+
+
+
+
+
+
+
+//status filter in discover page
+
+// Dropdown
+function stat_filter_drop() {
+  var x = document.getElementById("stat_filter");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+
+
+
+
+
+
+//ort filter in discover page
+
+// Dropdown
+function ort_filter_drop() {
+  var x = document.getElementById("ort_filter");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+
+
+
+
+
+
+
+// share links
+function FBshare() {
+  window.open("https://www.facebook.com/sharer.php?title="+document.title+"u="+document.URL);
+}
+
+function TTshare() {
+  window.open("https://twitter.com/intent/tweet?text="+document.title+"&url="+document.URL);
+}
+
+function WAshare() {
+  window.open("https://web.whatsapp.com/send?text="+document.title+" | "+document.URL);
+}
+
+function Emailshare() {
+  window.open("mailto:?subject="+document.title+"&body="+document.URL);
+}
+
+
+
+
+
+
+
+//related tab
+
+function relatedtab(evt, tab) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-green", "");
+  }
+  document.getElementById(tab).style.display = "block";
+  evt.currentTarget.className += " w3-green";
+}
+
+
+
+
+
+
+// User account Accordion
+function user_acc(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    x.previousElementSibling.className += " w3-theme-d1";
+  } else { 
+    x.className = x.className.replace("w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-theme-d1", "");
+  }
+}
--- photobay/settings.py
+++ photobay/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+STATIC_ROOT = BASE_DIR / 'my_CDN'
+
+LOGIN_URL = '/login/'
